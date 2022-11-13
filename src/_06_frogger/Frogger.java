57e0121c88c279bb67d95342a71f60321a3a3d54
league <league@192.168.69.13>
2022-11-13 14:47:14 -0800

    reeeeeeeeeee

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -3,12 +3,36 @@ package _06_frogger;
 import processing.core.PApplet;
 
 public class Frogger extends PApplet {
-    static final int WIDTH = 600;
-    static final int HEIGHT = 400;
+
+    int x_pos = 300;
+    int y_pos = 800;
+    int hop_Distance = 50;
+    
+    public void keyPressed()
+    {
+        if(key == CODED){
+            if(keyCode == UP && y_pos>0)
+            {
+            y_pos= y_pos-50;
+            }
+            else if(keyCode == DOWN && y_pos<801)
+            {
+            y_pos= y_pos+50;
+            }
+            else if(keyCode == RIGHT && x_pos<601)
+            {
+            x_pos= x_pos+50;
+            }
+            else if(keyCode == LEFT && x_pos>0)
+            {
+            x_pos= x_pos-50;
+            }
+        }
+    }
     
     @Override
     public void settings() {
-        size(WIDTH, HEIGHT);
+        size(600, 800);
     }
 
     @Override
@@ -18,9 +42,19 @@ public class Frogger extends PApplet {
 
     @Override
     public void draw() {
-
+    background(21,42,84);
+    fill(69, 220, 59);
+    ellipse(x_pos, y_pos, 50, 50);
+    fill(200, 50, 59);
+    rect(100,600, 200,100);
+    rect(400,400, 200,100);
+    rect(50,300, 200,100);
     }
     static public void main(String[] args) {
         PApplet.main(Frogger.class.getName());
     }
-}
+    
+ 
+    }
+    
+
---
+++ src/_06_frogger/Kar.java
@@ -0,0 +1,18 @@
+package _06_frogger;
+
+public class Kar {
+int W;
+int H;
+int X;
+int Y;
+int speed;
+
+public Kar(int X,int Y,int W,int H,int speed) {
+
+	
+}
+
+
+
+
+}
